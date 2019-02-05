2119c04b3f69393f415d60d812864d73b86ff822
reksar2012 <stenchev60@gmail.com>
2019-02-05 17:43:36 +0300

    add converter

---
+++ currency-converter.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+const getExchangeRate = async (fromCurrency, toCurrency) => {
+    try {
+      const response = await axios.get('http://www.apilayer.net/api/live?access_key=4b8614857b6521c042b5d7cee5a773ce');
+
+      const rate = response.data.quotes;
+      const baseCurrency = response.data.source;
+      const usd = 1 / rate[`${baseCurrency}${fromCurrency}`];
+      const exchangeRate = usd * rate[`${baseCurrency}${toCurrency}`];
+  
+      return exchangeRate;
+    } catch (error) {
+      throw new Error(`Unable to get currency ${fromCurrency} and ${toCurrency}`);
+    }
+};
+  
+const getCountries = async (currencyCode) => {
+    try {
+      const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+  
+      return response.data.map(country => country.name);
+    } catch (error) {
+      throw new Error(`Unable to get countries that use ${currencyCode}`);
+    }
+};
+  
+const convertCurrency = async (fromCurrency, toCurrency, amount) => {
+    const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
+    const countries = await getCountries(toCurrency);
+    const convertedAmount = (amount * exchangeRate).toFixed(2);
+  
+    return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
+};
+
+module.exports = convertCurrency;
